190d43dc74898e4960b8e9e73df68f599daa2abd
Takuto Mori Gump <mr04vv@gmail.com>
2024-12-31 21:13:33 +0900

    :wrench: chore: eslintのセッティングを修正

--- .config/nvim/lua/modules/completion/config.lua
+++ .config/nvim/lua/modules/completion/config.lua
@@ -41,7 +41,7 @@ function config.null_ls()
       null_ls.builtins.formatting.prettier.with({
         prefer_local = "node_modules/.bin",
       }),
-      null_ls.builtins.diagnostics.eslint,
+      require("none-ls.diagnostics.eslint"),
     },
     on_attach = function(client, bufnr)
       if client.supports_method("textDocument/formatting") then
--- .config/nvim/lua/modules/completion/init.lua
+++ .config/nvim/lua/modules/completion/init.lua
@@ -33,6 +33,7 @@ return {
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-tree/nvim-web-devicons",
+			"nvimtools/none-ls-extras.nvim",
 		},
 	},
 	{
