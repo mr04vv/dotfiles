36b17f2c98bc5c1c55d09057aee54213b417e4cc
mr04vv <mr04vv@gmail.com>
2026-02-28 17:31:10 +0900

    :wrench: chore: tmuxの設定を修正

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -6,7 +6,7 @@
     terminal = "screen-256color";
     keyMode = "vi";
     mouse = true;
-    prefix = "C-b";
+    prefix = "C-t";
     escapeTime = 0;
     historyLimit = 50000;
 
@@ -20,19 +20,34 @@
       set-window-option -g pane-base-index 1
       set-option -g renumber-windows on
 
-      # Split panes using | and -
-      bind | split-window -h -c "#{pane_current_path}"
-      bind - split-window -v -c "#{pane_current_path}"
+      # Split panes without prefix
+      bind -n C-\\ split-window -h -c "#{pane_current_path}"
+      bind -n C-- split-window -v -c "#{pane_current_path}"
       unbind '"'
       unbind %
+
+      # Kill pane without prefix
+      bind -n C-x kill-pane
+
+      # Pane navigation with Shift+Arrow
+      bind -n S-Left select-pane -L
+      bind -n S-Down select-pane -D
+      bind -n S-Up select-pane -U
+      bind -n S-Right select-pane -R
 
       # Reload config
       bind r source-file ~/.config/tmux/tmux.conf \; display "Config reloaded!"
 
+      # Display panes time
+      set -g display-panes-time 2000
+
       # Status bar
-      set -g status-style 'bg=default,fg=white'
-      set -g status-left-length 40
+      set -g status-bg black
+      set -g status-fg white
+      set -g status-left-length 60
+      set -g status-left "#[fg=green][#S] #[fg=yellow]C-t:Prefix | Shift+Arrow:Move "
       set -g status-right-length 60
+      set -g status-right "#[fg=cyan]C-\\:V-Split C--:H-Split #[fg=white]%H:%M"
     '';
   };
 }
