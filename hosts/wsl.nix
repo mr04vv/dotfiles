9be5185d8b163a3b015bbbbcc5cf873666653fa3
mr04vv <mr04vv@gmail.com>
2026-03-24 23:35:05 +0900

    :wrench: chore: fix for windowes

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -259,7 +259,23 @@
         { key = '{', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
         { key = '}', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(1) },
         { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+        { key = 'Enter', mods = 'SHIFT', action = act.SendString '\n' },
+        {
+          key = 'c',
+          mods = 'CTRL',
+          action = wezterm.action_callback(function(window, pane)
+            local selection = window:get_selection_text_for_pane(pane)
+            if selection ~= nil and selection ~= "" then
+              window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+              window:perform_action(act.ClearSelection, pane)
+            else
+              window:perform_action(act.SendKey({ key = 'c', mods = 'CTRL' }), pane)
+            end
+          end),
+        },
       }
+
+      config.enable_kitty_keyboard = true
 
       return config
     '';
